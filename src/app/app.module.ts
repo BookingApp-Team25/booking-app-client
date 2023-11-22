bc9a12d6eb0b5afe6eaf98814ddfa2b40a2ab875
Miroslav0222 <105170901+Miroslav0222@users.noreply.github.com>
2023-11-22 10:37:14 +0100

    Initial commit of empty working project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {LayoutModule} from "./layout/layout.module";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {WineModule} from "./wine/wine.module";
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutModule,
+    BrowserAnimationsModule,
+    WineModule
   ],
   providers: [],
   bootstrap: [AppComponent]
