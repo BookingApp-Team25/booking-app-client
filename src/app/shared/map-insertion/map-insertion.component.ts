5a7c158610f9221a1290b3763d07c585c9c59e2c
Miroslav0222 <105170901+Miroslav0222@users.noreply.github.com>
2024-01-15 13:47:43 +0100

    Implemented map view in accommodation creation module

---
+++ src/app/shared/map-insertion/map-insertion.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map-insertion',
+  templateUrl: './map-insertion.component.html',
+  styleUrls: ['./map-insertion.component.css']
+})
+export class MapInsertionComponent {
+
+}
