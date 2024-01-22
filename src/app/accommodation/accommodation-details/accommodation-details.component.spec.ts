fcd242fd7f174b9644f15bce1fe8c44aeab9cb4f
Stevan Sapun <stevan.sapun@gmail.com>
2024-01-22 23:15:42 +0100

    UnitTests

--- src/app/accommodation/accommodation-details/accommodation-details.component.spec.ts
+++ src/app/accommodation/accommodation-details/accommodation-details.component.spec.ts
@@ -1,12 +1,21 @@
 import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { AccommodationDetailsComponent } from './accommodation-details.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AccommodationService } from '../accommodation.service';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { RouterTestingModule } from '@angular/router/testing';
 import { throwError, of } from 'rxjs';
 import { MessageResponse } from '../model/message-response';
 import { AuthService } from '../../infrastructure/auth/auth.service';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { MatIconModule } from '@angular/material/icon';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatCardModule } from '@angular/material/card';
+import { MapInsertionComponent } from '../../shared/map-insertion/map-insertion.component';
 
 describe('AccommodationDetailsComponent', () => {
   let component: AccommodationDetailsComponent;
@@ -20,14 +29,32 @@ describe('AccommodationDetailsComponent', () => {
     getUsername: () => 'mockUsername',
   };
 
+  // Mock HttpClient
+  class HttpClientMock {}
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [AccommodationDetailsComponent],
-      imports: [ReactiveFormsModule, MatSnackBarModule, RouterTestingModule],
+      declarations: [AccommodationDetailsComponent, MapInsertionComponent],
+      imports: [
+        ReactiveFormsModule,
+        FormsModule,
+        MatSnackBarModule,
+        RouterTestingModule,
+        HttpClientModule,
+        MatIconModule,
+        MatFormFieldModule,
+        MatInputModule,
+        HttpClientTestingModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatCardModule,
+        MatIconModule,
+      ],
       providers: [
         AccommodationService,
         { provide: AuthService, useValue: authServiceMock },
-        MatSnackBar, // Include MatSnackBar directly as a provider
+        { provide: MatSnackBar, useValue: {} },
+        { provide: HttpClient, useClass: HttpClientMock },
       ],
     });
 
@@ -37,7 +64,7 @@ describe('AccommodationDetailsComponent', () => {
     snackBar = TestBed.inject(MatSnackBar);
     fixture.detectChanges();
   });
-  
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
@@ -47,10 +74,10 @@ describe('AccommodationDetailsComponent', () => {
     component.checkoutDate = new Date('2023-01-05');
     component.guests = 3;
 
-    spyOn(component, 'calculateTotalPrice').and.callThrough(); // Spy on the method to check if it's called
+    spyOn(component, 'calculateTotalPrice').and.callThrough();
 
     component.onCheckoutDateChange();
-    tick(); // ensure all asynchronous operations are completed
+    tick();
     component.onGuestsChange();
     tick();
 
@@ -59,7 +86,7 @@ describe('AccommodationDetailsComponent', () => {
 
   it('should allow guests to submit reservation requests', fakeAsync(() => {
     component.role = 'ROLE_GUEST';
-    const spyCreateReservation = spyOn(component.accommodationService, 'createReservation').and.returnValue(of({} as MessageResponse));
+    const spyCreateReservation = spyOn(accommodationService, 'createReservation').and.returnValue(of({} as MessageResponse));
 
     component.onReserveClicked('2023-01-01', '2023-01-05', '3');
     tick();
@@ -69,9 +96,9 @@ describe('AccommodationDetailsComponent', () => {
 
   it('should handle server error when creating reservation', fakeAsync(() => {
     component.role = 'ROLE_GUEST';
-    spyOn(component.accommodationService, 'createReservation').and.returnValue(throwError('Server error'));
+    spyOn(accommodationService, 'createReservation').and.returnValue(throwError('Server error'));
 
-    const snackBarOpenSpy = spyOn(component.snackBar, 'open');
+    const snackBarOpenSpy = spyOn(snackBar, 'open');
 
     component.onReserveClicked('2023-01-01', '2023-01-05', '3');
     tick();
@@ -80,15 +107,15 @@ describe('AccommodationDetailsComponent', () => {
   }));
 
   it('should display a confirmation message upon successful reservation', fakeAsync(() => {
-    (component as any).role = 'ROLE_GUEST'; // Accessing private property using as any
-    
+    component.role = 'ROLE_GUEST';
+
     const mockMessageResponse: MessageResponse = {
       successful: true,
-      message: 'Reservation created successfully'
+      message: 'Reservation created successfully',
     };
 
-    spyOn((component as any).accommodationService, 'createReservation').and.returnValue(of(mockMessageResponse));
-    const snackBarOpenSpy = spyOn((component as any).snackBar, 'open');
+    spyOn(accommodationService, 'createReservation').and.returnValue(of(mockMessageResponse));
+    const snackBarOpenSpy = spyOn(snackBar, 'open');
 
     component.onReserveClicked('2023-01-01', '2023-01-05', '3');
     tick();
